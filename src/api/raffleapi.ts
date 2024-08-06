89719cd96d5525e760e3ce4b8f66396de2a74870
MartinAntonio123 <mavp98@hotmail.com>
2024-08-05 20:00:38 -0600

    add env variables

--- src/api/raffleapi.ts
+++ src/api/raffleapi.ts
@@ -15,8 +15,7 @@ class APIRaffle {
 
   constructor() {
     this.axiosInstance = axios.create({
-      //baseURL: `${import.meta.env.VITE_CALTEN_API}/api`,
-      baseURL: `https://calten-raffle-back.vercel.app/api`,
+      baseURL: `${import.meta.env.VITE_CALTEN_API}/api`,
     });
   }
 
